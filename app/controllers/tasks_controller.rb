0a8b8aac571074ee0c6c0a2627804b37258c7d55
Khoa Le <khoatle165@gmail.com>
2019-08-06 14:14:46 +0200

    finish new, create, update tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,42 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    new_task = Task.create(task_strong_params)
+    redirect_to task_path(new_task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_strong_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_strong_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id].to_i)
+  end
 end
